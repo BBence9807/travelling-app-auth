f25af52112c14bcb49511098f958a6b30b9f209c
BBence0798 <bence.badics@gmail.com>
2024-02-11 12:34:35 +0100

    First Commit

---
+++ src/main/java/hu/bb/travellingappauth/TravellingAppAuthApplication.java
@@ -0,0 +1,13 @@
+package hu.bb.travellingappauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingAppAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingAppAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bb/travellingappauth/TravellingAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bb.travellingappauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravellingAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
